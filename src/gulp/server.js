fd309b4a1a2edae0ab85e7af490ef00f802f8693
lyudakucher <kucherlyuda@gmail.com>
2016-09-05 12:09:34 +0300

    fixing lifereload

--- src/gulp/server.js
+++ src/gulp/server.js
@@ -1,23 +1,37 @@
 /**
  * Created by air on 01.09.16.
  */
-var gulp = require('gulp');
-connect = require('gulp-connect');
+// var gulp = require('gulp');
+    // connect = require('gulp-connect');
+// var browserSync = require('browser-sync').create();
 
-gulp.task('connect', function() {
-    connect.server({
-        root: 'build',
-        livereload: true
-    });
-});
+// gulp.task('connect', function() {
+//     connect.server({
+//         root: 'build',
+//         livereload: true
+//     });
+// });
 
-gulp.task('html', function () {
-    gulp.src('./build/*.html')
-        .pipe(connect.reload());
-});
+// gulp.task('browserSync', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: 'build'
+//         },
+//     })
+// })
 
-gulp.task('watch', function () {
-    gulp.watch(['./build/*.html'], ['html']);
-});
+// gulp.task('html', function () {
+//     gulp.src('build/*.html')
+//         .pipe(connect.reload());
+// });
 
-gulp.task('default', ['connect', 'watch']);
+// gulp.task('sass', function () {
+//     gulp.src('build/**/*.css')
+//         .pipe(connect.reload());
+// });
+
+// gulp.task('connect:watch', function () {
+//     gulp.watch(['./build/*.html'], ['html']);
+// });
+
+// gulp.task('default', ['connect', 'watch']);
--- src/gulp/watch.js
+++ src/gulp/watch.js
@@ -2,10 +2,24 @@
  * Created by air on 01.09.16.
  */
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 
-gulp.task('watch',
-    [
-        'sass:watch',
-        'nunjucks:watch'
-    ]
-);
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'build'
+        },
+    })
+})
+
+// gulp.task('watch',
+//     [
+//         'sass:watch',
+//         'nunjucks:watch'
+//     ]
+// );
+
+gulp.task('watch', ['browserSync', 'sass:watch', 'nunjucks:watch'], function (){
+    gulp.watch('build/css/*.css', browserSync.reload);
+    gulp.watch('build/*.html', browserSync.reload);
+});
