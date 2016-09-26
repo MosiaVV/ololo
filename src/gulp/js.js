ca6f276b7f0e2224e7eb9ed3a1e850ce6d5e579d
sanchahous <sanchahous@gmail.com>
2016-09-26 12:58:01 +0300

    add jquery main.js created skills circle animated block

--- src/gulp/js.js
+++ src/gulp/js.js
@@ -1,6 +1,9 @@
-// var gulp = require('gulp');
-//
-// gulp.task('js', function () {
-//     gulp.src('src/js/**/*.js')
-//         .pipe(gulp.dest('build/js/'));
-// });
+var gulp = require('gulp');
+
+gulp.task('js', function () {
+    gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('build/js/'));
+});
+gulp.task('js:watch', function () {
+    gulp.watch('src/js/**/*.js', ['js']);
+});
